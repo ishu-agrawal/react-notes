5e11e113a8d4708af4bdeb6b2fc1c127678a246e
Utkarsh Agrawal <agrawalutkarsh4701@gmail.com>
2025-09-03 01:37:06 +0530

    dynamic data, CORS error

---
+++ lec-6/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const RES_IMG_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
+export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNl1WcEr1Wv0VlscNKDZKcM-x0WkVFKNn2Zg&s";
