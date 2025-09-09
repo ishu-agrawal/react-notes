a977a53aeac81809735f57f53c0d8722496091e8
Utkarsh Agrawal <agrawalutkarsh4701@gmail.com>
2025-09-10 01:44:09 +0530

    custom hooks, dynamic bundling

---
+++ lec-9/src/components/About.js
@@ -0,0 +1,15 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <h3>This is about page</h3>
+            < User name={'Utkarsh Agrawal'}/>
+            < UserClass name={'Utkarsh Agrawal'}/>
+        </div>
+    )
+}
+
+export default About;
