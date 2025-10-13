9759f37eb25479164c67f20b664e290043e1e9f0
Utkarsh Agrawal <agrawalutkarsh4701@gmail.com>
2025-10-14 01:01:37 +0530

    if user is logged in redirect to home page

--- netflixGPT/netflixGPT/src/components/Header.jsx
+++ netflixGPT/netflixGPT/src/components/Header.jsx
@@ -1,20 +1,42 @@
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
     const navigate = useNavigate()
     const user = useSelector(store => store.user);
+    const dispatch = useDispatch();
 
     const handleSignOut = () => {
         signOut(auth).then(() => {
-            navigate('/');
+            // navigate('/'); // handled in useEffect
         }).catch((error) => {
             console.log('error occured while signing out: ', error);
         });
 
     }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const {uid, email, displayName, photoURL} = user;
+                dispatch(addUser({
+                    uid: uid,
+                    email: email,
+                    displayName: displayName,
+                    photoURL: photoURL
+                }))
+                navigate('/browse');
+            } else {
+                dispatch(removeUser());
+                navigate('/');
+            }
+        });
+    }, []);
+    
     return (
         <div className="flex justify-between absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
             <img 
