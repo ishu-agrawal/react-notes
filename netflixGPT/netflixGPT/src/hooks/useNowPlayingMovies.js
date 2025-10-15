ae44b18a7008cbbe346b880ef560ce6492c1822e
Utkarsh Agrawal <agrawalutkarsh4701@gmail.com>
2025-10-15 10:41:45 +0530

    created movie reducer to maintain movie data

---
+++ netflixGPT/netflixGPT/src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux"
+import { API_CONFIG } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+
+const useNowPlayingMovies = () => {
+    const dispatch = useDispatch();
+
+    const fetchNowPlayingMovies = async() => {
+        try {
+            const res = await fetch("https://api.themoviedb.org/3/movie/now_playing", API_CONFIG);
+            const data = await res.json();
+            console.log(data);
+
+            dispatch(addNowPlayingMovies(data.results));
+        } catch (err) {
+            console.error("Error fetching movies:", err);
+        }
+    }
+
+    useEffect(() => {
+        fetchNowPlayingMovies();
+    }, []);
+}
+
+export default useNowPlayingMovies;
