69f0d90cdd89bdd43808d845327e5bfb17a36363
Utkarsh Agrawal <agrawalutkarsh4701@gmail.com>
2025-10-31 23:44:18 +0530

    displaying search results to user

--- netflixGPT/netflixGPT/src/components/gptComponent/GPTMovieSuggestion.jsx
+++ netflixGPT/netflixGPT/src/components/gptComponent/GPTMovieSuggestion.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import MovieList from '../browsePage/MovieList'
+import { useSelector } from 'react-redux'
 
 const GPTMovieSuggestion = () => {
+  const {movieResult} = useSelector((store) => store.gpt);
+  if(!movieResult) return;
   return (
-    <div>GPTMovieSuggestion</div>
+    <div className='p-4 m-4 bg-black opacity-90'>
+      <div>
+        {movieResult.map(
+            (movie, index) => (
+              <MovieList 
+                key={index} 
+                title={movie[0].primaryTitle} 
+                moviesData={movie} 
+              />
+            )
+          )
+        }
+        
+      </div>
+    </div>
   )
 }
 
